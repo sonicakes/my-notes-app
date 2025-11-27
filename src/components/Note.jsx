350a81f23e2265d8c757ebd81c6dbffb59b3151d
Sonia Parker <soniaparker1207@gmail.com>
2025-11-27 14:32:40 +1100

    pin/unpin hover states

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -5,28 +5,54 @@ import { LuPinOff } from "react-icons/lu";
 
 const Note = ({ note, deleteNote }) => {
   const [hover, setHover] = useState(false);
+
+  const baseClasses= `
+              cursor-pointer 
+              transition-colors
+               ${note.isPinned 
+                ? ''         // Pinned: Black outline, Black fill
+                : 'hover:fill-gray-500 hover:text-gray-500' // Unpinned: Grey outline, No fill
+              } 
+              ${note.isPinned 
+                ? 'text-gray-900 fill-gray-900'         // Pinned: Black outline, Black fill
+                : 'text-gray-500 fill-transparent' // Unpinned: Grey outline, No fill
+              }
+            `;
+
+
   return (
     //Tailwind cannot see classes that are constructed at runtime because it scans your code as plain text before the JavaScript runs.
     <div className={`rounded-lg ${note.color} p-4`}>
       <div className="flex justify-between">
         <h3 className="font-bold text-lg capitalize">{note.title}</h3>
         <div
-          // onMouseEnter={() => setHover(true)}
-          // onMouseLeave={() => setHover(false)}
-          // onClick={() => unpinNote(note.id, false)}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          //onClick={() => unpinNote(note.id, !note.isPinned)}
         >
-            {/* <LuPinOff className="cursor-pointer" size={20} fill="black" /> */}
-                  {note.isPinned ? (
-
-            <LuPin className="cursor-pointer" size={20} fill="black" />
-                  ) : ('') }
+      {/* Conditionally render the icon based on isHovered */}
+      {hover && note.isPinned ? (
+        
+        // Pin is Pinned (Action: Unpin) -> Show the PinOff icon in a visible hover color
+        <LuPinOff 
+          className={`${baseClasses} `} 
+          size={20} 
+        />
+      ) : (
+        
+        // Default state (or if Unpinned) -> Show the normal Pin icon
+        <LuPin 
+          className={`${baseClasses} `} 
+          size={20} 
+        />
+      )}
         </div>
       </div>
       <p>Category: {note.category}</p>
       <p>Priority: {note.priority}</p>
       <p>{note.description}</p>
       <div className="flex justify-end" onClick={() => deleteNote(note.id)}>
-        <LuTrash2 size={20} className="cursor-pointer hover:fill-gray-500" />
+        <LuTrash2 size={20} className="cursor-pointer text-gray-500 hover:fill-gray-500" />
       </div>
     </div>
   );
